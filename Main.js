fcb90c9be6e49d092657629db8769489b4d4dae1
Kunakbaev <53218382+Kunakbaev@users.noreply.github.com>
2020-04-20 16:21:24 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,7 +1,17 @@
+let value = 0;
 void setup(){
   createCanvas(600, 600);
   background(0);
 }
 void draw(){
   background(0);
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function touchStarted() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
 }
